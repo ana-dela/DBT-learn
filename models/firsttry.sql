85a0d2b2e26a5f05ca73aafb4d98a247dcb8ef86
Ana Dela <azadehdelavari.system@gmail.com>
2023-12-29 11:28:35 +0000

    gg

--- models/firsttry.sql
+++ models/firsttry.sql
@@ -1,6 +1,6 @@
-with cte as 
-(select *
-from  `dbt-tutorial.jaffle_shop.customers` as c
-left join  `dbt-tutorial.jaffle_shop.orders`  as o on c.ID = o.ID
-limit 3)
-select * from cte
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from dbt-tutorial.jaffle_shop.customers
